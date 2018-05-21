10f7bbb61e17363e375e0f0888b3089e33e78a26
Zach Schneider <zschneid85@yahoo.com>
2018-05-21 11:22:09 -0400

    todo-app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Foods from "./pages/Foods";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Todo from "./components/ToDo/wrappers/App";
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
       <Nav />
       <Switch>
         <Route exact path="/" component={Foods} />
+        <Route exact path="/todo" component={Todo} />
         <Route exact path="/foods" component={Foods} />
         <Route exact path="/foods/:id" component={Detail} />
         <Route component={NoMatch} />
